a9fdbe62bea44a6ea52e03d8961d437ecded7cbb
Bartłomiej Pluta <the.orleander@gmail.com>
2019-07-05 21:33:04 +0200

    Add support for properties to list expressions

--- smnp/newast/node/list.py
+++ smnp/newast/node/list.py
@@ -1,10 +1,11 @@
+from smnp.newast.node.access import AccessNode
 from smnp.newast.node.expression import ExpressionNode
 from smnp.newast.node.iterable import abstractIterableParser
 from smnp.token.type import TokenType
 
 
-class ListNode(ExpressionNode):
+class ListNode(AccessNode):
 
     @classmethod
-    def _parse(cls, input):
-        return abstractIterableParser(ListNode, TokenType.OPEN_SQUARE, TokenType.CLOSE_SQUARE, ExpressionNode.parse)(input)
+    def _literalParser(cls):
+        return abstractIterableParser(ListNode, TokenType.OPEN_SQUARE, TokenType.CLOSE_SQUARE, ExpressionNode.parse)
