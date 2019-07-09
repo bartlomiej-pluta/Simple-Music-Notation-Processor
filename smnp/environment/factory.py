67d05c23bed98b8106813e66ae76f101263fdd9d
Bartłomiej Pluta <the.orleander@gmail.com>
2019-07-09 23:09:54 +0200

    Clean environment factory method

--- smnp/environment/factory.py
+++ smnp/environment/factory.py
@@ -4,35 +4,6 @@ from smnp.type.model import Type
 
 
 def createEnvironment():
-    # functions = [
-    #     display.function,
-    #     type.function,
-    #     exit.function,
-    #     sleep.function,
-    #     semitones.function,
-    #     interval.function,
-    #     combine.function,
-    #     flat.function,
-    #     #wait.function,
-    #     rand.function,
-    #     tuplet.function,
-    #     synth.function,
-    #     pause.function,
-    #     transpose.function,
-    #     sound.function,
-    #     map.function,
-    #     concat.function,
-    #     range.function,
-    #     debug.function
-    # ]
-    #
-    # methods = [
-    #     duration.function,
-    #     octave.function,
-    #     play.function,
-    #     get.function
-    # ]
-
     variables = {
         "bpm": Type.integer(120)
     }
