2737139962c7141012e4e2d026890a00ab924234
Bartłomiej Pluta <the.orleander@gmail.com>
2019-07-14 00:13:05 +0200

    Clear main

--- smnp/main.py
+++ smnp/main.py
@@ -1,16 +1,14 @@
 import sys
 
 from smnp.error.base import SmnpException
+from smnp.library.loader import loadStandardLibrary
 from smnp.program.interpreter import Interpreter
 
 
 def main():
     try:
-        #stdLibraryEnv = loadStandardLibrary()
-        Interpreter.interpretFile(sys.argv[1], printTokens=False, printAst=False, execute=True, baseEnvironment=None)
-        #draft()
-        #tokens = tokenize(['function a(b...) { x+y}'])
-        #FunctionDefinitionParser(tokens).node.print()
+        stdLibraryEnv = loadStandardLibrary()
+        Interpreter.interpretFile(sys.argv[1], printTokens=False, printAst=False, execute=True, baseEnvironment=stdLibraryEnv)
 
     except SmnpException as e:
         print(e.message())
