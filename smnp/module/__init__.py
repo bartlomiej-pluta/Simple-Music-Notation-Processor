c9a3fc070b781511c8eae23c423f6adaaaade610
Bartłomiej Pluta <the.orleander@gmail.com>
2019-07-26 20:59:41 +0200

    Add Integer function to convert strings and floats to integers

--- smnp/module/__init__.py
+++ smnp/module/__init__.py
@@ -1,4 +1,4 @@
-from smnp.module import system, mic, note, iterable, sound, synth, string, util
+from smnp.module import system, mic, note, iterable, sound, synth, string, util, integer
 
-functions = [ *system.functions, *mic.functions, *note.functions, *iterable.functions, *sound.functions, *synth.functions, *string.functions, *util.functions ]
-methods = [ *system.methods, *mic.methods, *note.methods, *iterable.methods, *sound.methods, *synth.methods, *string.methods, *util.methods ]
+functions = [ *system.functions, *mic.functions, *note.functions, *iterable.functions, *sound.functions, *synth.functions, *string.functions, *util.functions, *integer.functions ]
+methods = [ *system.methods, *mic.methods, *note.methods, *iterable.methods, *sound.methods, *synth.methods, *string.methods, *util.methods, *integer.methods ]
